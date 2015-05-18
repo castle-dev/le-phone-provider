ec9fa2faf5f3f06ed35b6bbeb307db57a8d79aac
Scott Lowe <scott@entercastle.com>
2015-05-18 13:42:02 -0400

    feat(PhoneService): add text method
    Includes unit and e2e tests.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-module.exports = {};
+var PhoneService = function (provider, storage, from) {
+  if (!provider) { throw new Error('Phone provider required'); }
+  if (!storage) { throw new Error('Instance of le-storage-service required'); }
+  if (!from) { throw new Error('From phone number required'); }
+  var _provider = provider;
+  var _storage = storage;
+  var _from = from;
+
+  this.text = function (to, message) {
+    return _provider.text(_from, to, message)
+    .then(function () {
+      var record = _storage.createRecord('Text');
+      return record.update({
+        from: _from,
+        to: to,
+        message: message
+      }).then(function () { return record; });
+    });
+  }
+}
+
+module.exports = PhoneService;
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,15 +1,51 @@
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
-var chaiAsPromised = require("chai-as-promised");
+var chaiAsPromised = require('chai-as-promised');
+var sinonAsPromised = require('sinon-as-promised');
 var expect = chai.expect;
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
+var PhoneService = require('../../src/index.js');
+
 describe('unit tests::', function () {
+  var service;
+  var mockProvider =  { text: sinon.stub().resolves() };
+  var mockStorage = {
+    createRecord: sinon.stub().returns({
+      update: sinon.stub().resolves()
+    })
+  };
+  var from = '9188675309';
   it('should respect logic', function () {
     expect(true).to.be.true;
     expect(true).not.to.be.false;
   });
-  //TODO: add unit tests here
+  it('should be constructable', function () {
+    expect(function () {
+      service = new PhoneService(mockProvider, mockStorage, from);
+    }).not.to.throw();
+  });
+  it('should require a provider', function () {
+    expect(function () {
+      new PhoneService();
+    }).to.throw('Phone provider required');
+  });
+  it('should require an instance of le-storage-service', function () {
+    expect(function () {
+      new PhoneService(mockProvider);
+    }).to.throw('Instance of le-storage-service required');
+  });
+  it('should require a from phone number', function () {
+    expect(function () {
+      new PhoneService(mockProvider, mockStorage);
+    }).to.throw('From phone number required');
+  });
+  it('should send and store texts', function () {
+    var promise = service.text('1011011010', 'Hello')
+    .then(function (record) { expect(record.update).to.have.been.called });
+    expect(mockProvider.text).to.have.been.called;
+    return expect(promise).to.eventually.be.fulfilled;
+  });
 });
